7296a706ed3162c2100d30fe07fefe4801815974
Edvin Hansson <edvin.hansson+github@protonmail.com>
2023-12-06 08:34:22 +0100

    Remove unused function

--- customer.h
+++ customer.h
@@ -10,7 +10,6 @@ class Customer {
 public:
     Customer();
     Customer(int paid, bool active, const Message* messages, int numMessages);
-    void initCustomerMessages(Customer customerList[], const int numCustomers);
     bool isActive() const;
     int getPay() const;
     const Message& getNextMessage(bool isOddMinute) const;
